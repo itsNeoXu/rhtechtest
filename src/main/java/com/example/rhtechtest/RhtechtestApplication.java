27450fa2a1b62fc053a04c6573187e6ec275a110
Orhan Unal <orhanunal1990@gmail.com>
2025-03-22 12:13:47 +0000

    initial commit

---
+++ src/main/java/com/example/rhtechtest/RhtechtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhtechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhtechtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhtechtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rhtechtest/RhtechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rhtechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhtechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
